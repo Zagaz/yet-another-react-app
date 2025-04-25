f3c9d782a59f8e95b7bc944f7ffad9252f118b3e
Andre <andre_ranulfo@hotmail.com>
2025-04-25 00:25:28 -0300

    Hero ->Props: Title and subtitle

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+
 
 const App = () => {
 
   return (
     <>
     <Navbar />
+    <Hero title = "My Title" subtitle = "My Subtitle"  />
   
   
 
