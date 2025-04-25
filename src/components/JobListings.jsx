8654ae3b25977e2217563aa3aaf2b3f1850a9ff3
Andre <andre_ranulfo@hotmail.com>
2025-04-25 18:16:00 -0300

    <JobListing /> ok-2

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -16,7 +16,7 @@ const JobListings = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {/* Job Cards */}
         {jobs.map((job) => (
-        <JobListing job = {job} />
+        <JobListing  key={`job-${job.id}`}    job = {job} />
         ))}
       
 
