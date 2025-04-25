4260c68e37146323eb4b898476ee375d02a58ba0
Andre <andre_ranulfo@hotmail.com>
2025-04-25 20:03:29 -0300

    Viea all jobs button

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
 
 
 
@@ -10,30 +11,12 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <Hero
-        title="For Developers"
-        subtitle="Browse our React jobs and start your career today"
+      <Hero title="For Developers" subtitle="Browse our React jobs and start your career today"
       />
       <HomeCards />
       <JobListings />
+      <ViewAllJobs />
 
-
-
-      {/* Two cards */}
-
-
-      {/* Browse Jobs */}
-
-
-      {/* View all jobs */}
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
     </>
   )
 }
