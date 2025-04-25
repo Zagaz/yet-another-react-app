b1d4495691224a5de3350083d6fbb2b41d448ff4
Andre <andre_ranulfo@hotmail.com>
2025-04-25 20:50:22 -0300

    Inplementing Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
+import { Route, 
+        createBrowserRouter,
+        createRoutesFromElements,
+        RouterProvider
+      } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListings from './components/JobListings'
 import ViewAllJobs from './components/ViewAllJobs'
 
-
-
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route index element={<h1>My App</h1>} />
+  )
+) 
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero title="For Developers" subtitle="Browse our React jobs and start your career today"
-      />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-
-    </>
-  )
+  return <RouterProvider router = {router} />
+  
 }
 
 export default App
