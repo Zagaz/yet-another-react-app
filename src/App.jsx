919869d39d04caace7940ca00a6ea07188ce8542
Andre <andre_ranulfo@hotmail.com>
2025-04-26 16:39:37 -0300

    Job Page created and links working

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import HomePage from './pages/HomePage'
 import JobsPage from './pages/JobsPage' 
 import NotFoundPage from './pages/NotFoundPage' 
 import MainLayout from './layouts/MainLayout'
+import JobPage from './pages/JobPage'
 
 
 const router = createBrowserRouter(
@@ -15,6 +16,7 @@ const router = createBrowserRouter(
     <Route path='/' element = {<MainLayout />}>
       <Route index element = {<HomePage />} />
       <Route path='/jobs' element = {<JobsPage />} />
+      <Route path='/jobs/:id' element = {<JobPage />} />
       <Route path='/*' element = {<NotFoundPage />} />
     </Route>
 
