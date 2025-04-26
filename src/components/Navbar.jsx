e73d61ab0a618683f9a3e0fce1c383f007936d05
Andre <andre_ranulfo@hotmail.com>
2025-04-26 16:03:56 -0300

    Keep navbar stiky

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   
   return (
     <>
-      <nav className="bg-indigo-700 border-b border-indigo-500">
+      <nav className="bg-indigo-700 border-b border-indigo-500 sticky top-0 z-50">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="flex h-20 items-center justify-between">
             <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
